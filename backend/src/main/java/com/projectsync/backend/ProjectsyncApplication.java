632e736d0627c3ca333a755d072f3bd8759b475a
Carturo8 <caarojasbo@unal.edu.co>
2025-10-24 23:22:23 -0500

    Backend: Initialize Spring Boot project structure AB#6744

---
+++ backend/src/main/java/com/projectsync/backend/ProjectsyncApplication.java
@@ -0,0 +1,13 @@
+package com.projectsync.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsyncApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/projectsync/backend/ProjectsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsync.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
