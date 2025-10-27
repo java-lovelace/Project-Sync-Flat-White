dfa50dd725160967f6d5b4a167de03e4dda90b13
xguerrerov0903 <xguerrerov0903@gmail.com>
2025-10-27 08:22:00 -0500

    pimero

---
+++ backend/src/test/java/com/projectsync/backend/model/project.java
@@ -0,0 +1,11 @@
+package com.projectsync.backend.model;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectTest {
+    @Test
+
+}
